a15c63ac891c7004439e446d64c45d9ecbb8dc5d
Roberto Cervantes <robece@protonmail.com>
2020-03-12 16:29:12 -0600

    Update deploy.sh

--- challenge-01/configuration/deploy.sh
+++ challenge-01/configuration/deploy.sh
@@ -5,7 +5,7 @@ clear
 
 read -p "Introduce a lowercase unique alias for your deployment (max length suggested of 6 chars): " DeploymentAlias
 read -p "Introduce your email address in lowercase for your deployment: " EmailAddress
-ResourceGroupName=$DeploymentAlias
+ResourceGroupName=$DeploymentAlias"-workshop"
 Location="westus2"
 AKSClusterName=$DeploymentAlias"aks01"
 AKSK8sVersion="1.14.8"
