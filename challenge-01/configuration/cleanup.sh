a5f00932a97bff78b82223f109a49de73bef9af2
Roberto Cervantes <robece@protonmail.com>
2020-03-15 00:11:33 -0600

    Update cleanup.sh

--- challenge-01/configuration/cleanup.sh
+++ challenge-01/configuration/cleanup.sh
@@ -7,15 +7,16 @@ read -p "Introduce a lowercase unique alias of the deployment you want to cleanu
 ResourceGroupName=$DeploymentAlias"-microservices-aks-c01"
 
 # get location
-LOCATION=$(az group show -n $ResourceGroupName --query id -o tsv)
-AKSResourceNodeGroupName="MC_"$ResourceGroupName"_"$DeploymentAlias"aks01_"$LOCATION
+LOCATION=$(az group show -n $ResourceGroupName --query location -o tsv)
+ResourceNodeGroupName="MC_"$ResourceGroupName"_"$DeploymentAlias"aks01_"$LOCATION
 
+echo "Delete resource group "$ResourceGroupName 
 # delete delete general resource group
 az group delete -n $ResourceGroupName --no-wait
 
-echo $AKSResourceNodeGroupName
+echo "Delete resource group "$ResourceNodeGroupName
 # delete delete aks node group resource group
-az group delete -n $AKSResourceNodeGroupName --no-wait
+az group delete -n $ResourceNodeGroupName --no-wait
 
 # get service principal
 SP_APP_ID=$(az ad sp show --id http://$ResourceGroupName"-sp" --query appId -o tsv)
